b7cff1a83dffa654710ddc8d186237d98c528060
Berkay Emre Aslan <emreaslan1177@gmail.com>
2024-12-30 15:37:02 +0300

    Adding photo picker

--- ChitChatApp/Screens/Chat/Views/TextInputArea.swift
+++ ChitChatApp/Screens/Chat/Views/TextInputArea.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct TextInputArea: View {
     
     @Binding var textMessage: String
-    let onSendHandler:() -> Void
+    
+    let actionHandler:(_ action: UserAction) -> Void
+    
     private var disableSendButton: Bool {
         return textMessage.isEmptyoOrWhitespace
     }
@@ -48,7 +50,7 @@ struct TextInputArea: View {
     
     private func imagePickerButton() -> some View {
         Button{
-            
+            actionHandler(.presentPhotoPicker)
         }label: {
             Image(systemName: "photo.on.rectangle")
                 .font(.system(size: 22))
@@ -72,7 +74,7 @@ struct TextInputArea: View {
     
     private func sendMessageButton() -> some View {
         Button{
-            onSendHandler()
+            actionHandler(.sendMessage)
         }label: {
             Image(systemName: "arrow.up")
                 .fontWeight(.heavy)
@@ -84,8 +86,15 @@ struct TextInputArea: View {
     }
 }
 
+extension TextInputArea {
+    enum UserAction {
+        case presentPhotoPicker
+        case sendMessage
+    }
+}
+
 #Preview {
-    TextInputArea(textMessage: .constant("")) {
+    TextInputArea(textMessage: .constant("")) { _ in
         
     }
 }
