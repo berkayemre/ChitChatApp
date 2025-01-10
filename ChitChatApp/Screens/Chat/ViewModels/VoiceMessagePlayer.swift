7332b783673d3223e9f7b98aef8deabd91fe9758
Berkay Emre Aslan <emreaslan1177@gmail.com>
2025-01-10 17:15:41 +0300

    Building a Custom Audio Player for Voice Messages

--- ChitChatApp/Screens/Chat/ViewModels/VoiceMessagePlayer.swift
+++ ChitChatApp/Screens/Chat/ViewModels/VoiceMessagePlayer.swift
@@ -11,12 +11,84 @@ import AVFoundation
 final class VoiceMessagePlayer: ObservableObject {
     
     private var player: AVPlayer?
+    private var currentURL: URL?
+    private var playerItem: AVPlayerItem?
+    private var playbackState = PlaybackState.stopped
+    private var currentTime = CMTime.zero
+    private var currentTimeObserver: Any?
+    
+    deinit {
+        tearDown()
+    }
     
     func playAudio(from url: URL) {
-        
+        if let currentURL = currentURL, currentURL == url {
+            resumePlaying()
+        } else {
+            currentURL = url
+            let playerItem = AVPlayerItem(url: url)
+            self.playerItem = playerItem
+            player = AVPlayer(playerItem: playerItem)
+            player?.play()
+            playbackState = .playing
+            observeCurrentPlayerTime()
+            observeEndOfPlayback()
+        }
     }
     
     func pauseAudio() {
-        
+        player?.pause()
+        playbackState = .paused
+    }
+    
+    func seek(to timeInterval: TimeInterval) {
+        guard let player = player else { return }
+        let targetTime = CMTime(seconds: timeInterval, preferredTimescale: 1)
+        player.seek(to: targetTime)
+    }
+    
+    private func observeCurrentPlayerTime() {
+        currentTimeObserver = player?.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 1), queue: DispatchQueue.main) {[weak self] time in
+            self?.currentTime = time
+        }
+    }
+    
+    private func observeEndOfPlayback() {
+        NotificationCenter.default.addObserver(forName: AVPlayerItem.didPlayToEndTimeNotification, object: player?.currentItem, queue: .main) {[weak self] _ in
+            self?.stopAudioPlayer()
+        }
+    }
+    
+    private func stopAudioPlayer() {
+        player?.pause()
+        player?.seek(to: .zero)
+        playbackState = .stopped
+        currentTime = .zero
+    }
+    
+    private func removeObservers() {
+        guard let currentTimeObserver else { return }
+        player?.removeTimeObserver(currentTimeObserver)
+        self.currentTimeObserver = nil
+    }
+    
+    private func tearDown() {
+        removeObservers()
+        player = nil
+        playerItem = nil
+        currentURL = nil
+    }
+    
+    private func resumePlaying() {
+        if playbackState == .paused || playbackState == .stopped {
+            player?.play()
+            playbackState = .playing
+        }
+    }
+}
+
+extension VoiceMessagePlayer {
+    enum PlaybackState {
+        case stopped, playing, paused
     }
 }
