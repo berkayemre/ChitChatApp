76f67cf17eaee98a59e62b7529f7694d4049f016
Berkay Emre Aslan <emreaslan1177@gmail.com>
2025-02-17 22:04:42 +0300

    Setting up our App to Receive Push Notifications

--- ChitChatApp/ChitChatAppApp.swift
+++ ChitChatApp/ChitChatAppApp.swift
@@ -11,12 +11,53 @@ import Firebase
 
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
+    func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
+        FirebaseApp.configure()
+        setUpPushNotifications(for: application)
+        return true
+    }
+    
+    private func setUpPushNotifications(for application: UIApplication) {
+        let notificationCenter = UNUserNotificationCenter.current()
+        Messaging.messaging().delegate = self
+        notificationCenter.delegate = self
+        let options: UNAuthorizationOptions = [.alert, .badge, .sound]
+        
+        notificationCenter.requestAuthorization(options: options) { granted, error in
+            
+            if let error = error {
+                print("APNs Failed to request Authorization: \(error.localizedDescription)")
+                return
+            }
+            
+            if granted {
+                print("APNs Authorization granted")
+                DispatchQueue.main.async {
+                    application.registerForRemoteNotifications()
+                }
+            } else {
+                print("APNs Authorization was denied")
+            }
+        }
+    }
+}
 
-    return true
-  }
+extension AppDelegate: UNUserNotificationCenterDelegate, MessagingDelegate {
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("APNs device token is: \(fcmToken)")
+
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+        print("APNs successfully registered for push notifications: \(deviceToken)")
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
+        return [.badge, .banner, .sound]
+    }
 }
 
 @main
